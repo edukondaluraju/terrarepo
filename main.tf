82ee33794b539b3549848bf93fbc55f0a58d4565
edukondaluraju <116778866+edukondaluraju@users.noreply.github.com>
2022-12-18 23:14:02 +0530

    terra

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIASF3SZWWQ2DDRHEMV"
+  secret_key = "DueP1r4ZqGrkhHTdK+ZCPADDA33XPN/FFsVVGgvX"
 }
 
 resource "aws_instance" "example" {
